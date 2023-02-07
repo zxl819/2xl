ffea2ad7dc3070175fb22931f2f83c303189315b
zxlcreate <18519889511@163.com>
2023-02-07 22:34:43 +0800

    23.2.7 hash application of defaultdict

--- algorithm/hash.py
+++ algorithm/hash.py
@@ -5,3 +5,18 @@ for i,element in enumerate(seq):
 
 print(list(enumerate(seq)))
 
+#哈希表+排序
+# defaultdict(list),会构建一个默认value为list的字典
+'''
+#from collections import defaultdict
+result = defaultdict(list)
+data = [("p", 1), ("p", 2), ("p", 3),
+        ("h", 1), ("h", 2), ("h", 3)]
+ 
+for (key, value) in data:
+    result[key].append(value)
+print(result)#defaultdict(<class 'list'>, {'p': [1, 2, 3], 'h': [1, 2, 3]})
+'''
+
+
+
